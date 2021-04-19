7a2e3e0b5b921bd170f0afe663483eb81297e6e4
ghostviper235 <borkaranand19@siesgst.ac.in>
2021-04-19 18:37:06 +0530

    Recycler View added

--- app/src/main/java/com/sies/cyber/fragments/HomeFragment.java
+++ app/src/main/java/com/sies/cyber/fragments/HomeFragment.java
@@ -1,13 +1,23 @@
 package com.sies.cyber.fragments;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.sies.cyber.R;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import static android.widget.GridLayout.VERTICAL;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,6 +34,10 @@ public class HomeFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    RecyclerView mRecyclerView;
+    ArrayList<DataModel> dataHolder;
+
 
     public HomeFragment() {
         // Required empty public constructor
@@ -50,16 +64,67 @@ public class HomeFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
+    @SuppressLint("WrongConstant")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        mRecyclerView = view.findViewById(R.id.topic_recyclerview);
+        //GridLayoutManager gridLayoutManager = new GridLayoutManager(this,2, GridLayoutManager.VERTICAL,false);
+        //mRecyclerView.setLayoutManager(gridLayoutManager);
+        //mRecyclerView.setHasFixedSize(true);
+        //mRecyclerView.setAdapter(adapter);
+
+        mRecyclerView.setLayoutManager(new GridLayoutManager(getContext(),2,VERTICAL,false));
+        dataHolder = new ArrayList<>();
+
+
+        DataModel ob1 = new DataModel(R.drawable.ic_baseline_architecture_24,"First Topic");
+        dataHolder.add(ob1);
+
+        DataModel ob2 = new DataModel(R.drawable.ic_baseline_beenhere_24,"Second Topic");
+        dataHolder.add(ob2);
+
+        DataModel ob3 = new DataModel(R.drawable.ic_baseline_backpack_24,"Third Topic");
+        dataHolder.add(ob3);
+
+        DataModel ob4 = new DataModel(R.drawable.ic_baseline_assignment_ind_24,"Fourth Topic");
+        dataHolder.add(ob4);
+
+        DataModel ob5 = new DataModel(R.drawable.ic_baseline_architecture_24,"Fifth Topic");
+        dataHolder.add(ob5);
+
+        DataModel ob6 = new DataModel(R.drawable.ic_baseline_beenhere_24,"Sixth Topic");
+        dataHolder.add(ob6);
+
+        DataModel ob7 = new DataModel(R.drawable.ic_baseline_backpack_24,"Seventh Topic");
+        dataHolder.add(ob7);
+
+        DataModel ob8 = new DataModel(R.drawable.ic_baseline_assignment_ind_24,"Eight Topic");
+        dataHolder.add(ob8);
+
+        DataModel ob9 = new DataModel(R.drawable.ic_baseline_architecture_24,"Ninth Topic");
+        dataHolder.add(ob9);
+
+        DataModel ob10 = new DataModel(R.drawable.ic_baseline_beenhere_24,"Tenth Topic");
+        dataHolder.add(ob10);
+
+        DataModel ob11 = new DataModel(R.drawable.ic_baseline_backpack_24,"Eleventh Topic");
+        dataHolder.add(ob11);
+
+        DataModel ob12 = new DataModel(R.drawable.ic_baseline_assignment_ind_24,"Twelfth Topic");
+        dataHolder.add(ob12);
+
+        mRecyclerView.setAdapter(new myAdapter(dataHolder));
+
+        return view;
     }
 }
---
+++ app/src/main/java/com/sies/cyber/fragments/myAdapter.java
@@ -0,0 +1,52 @@
+package com.sies.cyber.fragments;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.sies.cyber.R;
+
+import java.util.ArrayList;
+
+public class myAdapter extends RecyclerView.Adapter<myAdapter.myViewHolder>{
+
+    ArrayList<DataModel> dataHolder;
+
+    public myAdapter(ArrayList<DataModel> dataHolder) {
+        this.dataHolder = dataHolder;
+    }
+
+    @NonNull
+    @Override
+    public myViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.topic_grid,parent,false);
+        return new myViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull myViewHolder holder, int position) {
+        holder.image.setImageResource(dataHolder.get(position).getImage());
+        holder.title.setText(dataHolder.get(position).getTitle());
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataHolder.size();
+    }
+
+    class myViewHolder extends RecyclerView.ViewHolder {
+        ImageView image;
+        TextView title;
+
+        public myViewHolder(@NonNull View itemView) {
+            super(itemView);
+            image = itemView.findViewById(R.id.imageView);
+            title = itemView.findViewById(R.id.textView);
+        }
+    }
+}
