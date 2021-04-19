fbbac1967abc889e9951a51b53dec5846cc8f869
Dark-Knight11 <vedantbhamare11@gmail.com>
2021-04-19 16:36:52 +0530

    Initial commit

---
+++ app/src/main/java/com/sies/cyber/MainActivity.java
@@ -0,0 +1,33 @@
+package com.sies.cyber;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+
+import com.sies.cyber.fragments.HomeFragment;
+import com.sies.cyber.fragments.SettingsFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        View settings = findViewById(R.id.page_2);
+        View home = findViewById(R.id.page_1);
+
+
+        // Begin the transaction
+        FragmentManager ft = getSupportFragmentManager();
+        // FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        // Replace the contents of the container with the new fragment
+        ft.beginTransaction().add(R.id.fl_wrapper, new HomeFragment(), "Home").commit();
+        // or ft.add(R.id.your_placeholder, new FooFragment());
+        // Complete the changes added
+        settings.setOnClickListener(v -> ft.beginTransaction().replace(R.id.fl_wrapper, new SettingsFragment(), "Settings").commit());
+        home.setOnClickListener(v -> ft.beginTransaction().replace(R.id.fl_wrapper, new HomeFragment(), "Home").commit());
+    }
+}
