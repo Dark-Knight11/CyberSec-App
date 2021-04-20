bfe8ed8a6b51221db9aca94bcbd3c4396bda5cca
ghostviper235 <borkaranand19@siesgst.ac.in>
2021-04-20 19:42:33 +0530

    Adding RecyclerViews to activity_topics

--- app/src/main/java/com/sies/cyber/Topic_1.java
+++ app/src/main/java/com/sies/cyber/Topic_1.java
@@ -3,12 +3,49 @@ package com.sies.cyber;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.sies.cyber.fragments.DataModel;
+import com.sies.cyber.fragments.myAdapter;
+
+import java.util.ArrayList;
+
+import static android.widget.GridLayout.VERTICAL;
 
 public class Topic_1 extends AppCompatActivity {
+    RecyclerView mRecyclerView;
+    ArrayList<DataModelInner> dataHolder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_topic_1);
+        mRecyclerView = findViewById(R.id.recView1);
+
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        dataHolder = new ArrayList<>();
+
+
+        DataModelInner ob1 = new DataModelInner("Heading1","Description1");
+        dataHolder.add(ob1);
+
+        DataModelInner ob2 = new DataModelInner("Heading2","Description2");
+        dataHolder.add(ob2);
+
+        DataModelInner ob3 = new DataModelInner("Heading3","Description3");
+        dataHolder.add(ob3);
+
+        DataModelInner ob4 = new DataModelInner("Heading4","Description4");
+        dataHolder.add(ob4);
+
+        DataModelInner ob5 = new DataModelInner("Heading5","Description5");
+        dataHolder.add(ob5);
+
+
+
+
+        mRecyclerView.setAdapter(new myAdapterInner(dataHolder));
     }
 }
---
+++ app/src/main/java/com/sies/cyber/myAdapterInner.java
@@ -0,0 +1,71 @@
+package com.sies.cyber;
+
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.sies.cyber.R;
+import com.sies.cyber.Topic_1;
+import com.sies.cyber.Topic_2;
+
+import java.util.ArrayList;
+
+public class myAdapterInner extends RecyclerView.Adapter<myAdapterInner.myViewHolder>{
+
+    ArrayList<DataModelInner> dataHolder;
+
+    public myAdapterInner(ArrayList<DataModelInner> dataHolder) {
+        this.dataHolder = dataHolder;
+    }
+
+    @NonNull
+    @Override
+    public myViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_topic_example,parent,false);
+        return new myViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull myViewHolder holder, int position) {
+        holder.title.setText(dataHolder.get(position).getTitle());
+        holder.description.setText(dataHolder.get(position).getTitle());
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataHolder.size();
+    }
+
+    class myViewHolder extends RecyclerView.ViewHolder {
+        TextView title;
+        TextView description;
+
+        public myViewHolder(@NonNull View itemView) {
+            super(itemView);
+            title = itemView.findViewById(R.id.activity_topic_title);
+            description = itemView.findViewById(R.id.activity_topic_description);
+
+
+            itemView.setOnClickListener(view -> Toast.makeText(view.getContext(),"Clicked on " + getAdapterPosition(),Toast.LENGTH_SHORT).show());
+//            itemView.setOnClickListener(this::onClick);
+        }
+
+//        private void onClick(View view) {
+//            switch (getAdapterPosition()) {
+//                case 0:
+//                    view.getContext().startActivity(new Intent(view.getContext(), Topic_1.class));
+//                    break;
+//                case 1:
+//                    view.getContext().startActivity(new Intent(view.getContext(), Topic_2.class));
+//                    break;
+//            }
+//        }
+    }
+}
