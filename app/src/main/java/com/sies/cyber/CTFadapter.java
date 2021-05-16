f91cf63fa98d939490614ff2711c768915fd65b9
ghostviper235 <borkaranand19@siesgst.ac.in>
2021-05-17 03:20:31 +0530

    Adding expanding function for description of CTFs and adding link to the name of the CTF

--- app/src/main/java/com/sies/cyber/CTFadapter.java
+++ app/src/main/java/com/sies/cyber/CTFadapter.java
@@ -1,10 +1,11 @@
 package com.sies.cyber;
 
-import android.content.Intent;
-import android.net.Uri;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -15,6 +16,8 @@ import java.util.List;
 
 public class CTFadapter extends RecyclerView.Adapter<CTFadapter.myViewHolder>{
     List<CtfsInfo> res;
+    int count = 0;
+    RelativeLayout desc;
 
     public CTFadapter(List<CtfsInfo> res) {
         this.res = res;
@@ -30,9 +33,30 @@ public class CTFadapter extends RecyclerView.Adapter<CTFadapter.myViewHolder>{
     @Override
     public void onBindViewHolder(@NonNull myViewHolder holder, int position) {
         holder.start_time.setText(res.get(position).getStart());
-        holder.main_link.setText(res.get(position).getTitle());
+        holder.main_link.setMovementMethod(LinkMovementMethod.getInstance());
+        holder.main_link.setText(Html.fromHtml(getHyperLink(res.get(position).getTitle(),res.get(position).getCtftime_url())));
+        holder.main_link.setMovementMethod(LinkMovementMethod.getInstance());
         holder.end_time.setText(res.get(position).getFinish());
-        holder.url = res.get(position).getCtftime_url();
+        holder.description.setText(res.get(position).getDescription());
+    }
+
+    private String getHyperLink(String title, String ctftime_url) {
+
+        String str = "<a href='"+ctftime_url+"'>"+title+"</a>";
+        return str;
+
+//            SpannableString str = new SpannableString(title);
+//            ClickableSpan span1 = new ClickableSpan() {
+//                @Override
+//                public void onClick(@NonNull View widget) {
+//                    String url = ctftime_url; // link to ctf
+//                    // Intent i = new Intent(Intent.ACTION_VIEW);
+//            i.setData(Uri.parse(url));
+//            view.getContext().startActivity(i);
+//                }
+//            };
+//            str.setSpan(span1 ,0, str.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+//            ctfName
     }
 
     @Override
@@ -41,20 +65,29 @@ public class CTFadapter extends RecyclerView.Adapter<CTFadapter.myViewHolder>{
     }
 
     class myViewHolder extends RecyclerView.ViewHolder {
-        TextView start_time,main_link,end_time;
+        TextView start_time,main_link,end_time,description;
+        RelativeLayout description_box;
         String url;
         public myViewHolder(@NonNull View itemView) {
             super(itemView);
             start_time = itemView.findViewById(R.id.start_time);
             main_link = itemView.findViewById(R.id.main_link);
             end_time = itemView.findViewById(R.id.end_time);
+            description = itemView.findViewById(R.id.description);
+            description_box = itemView.findViewById(R.id.description_box);
             itemView.setOnClickListener(this::onClick);
         }
 
         private void onClick(View view) {
-            Intent i = new Intent(Intent.ACTION_VIEW);
-            i.setData(Uri.parse(url));
-            view.getContext().startActivity(i);
+            if (count%2==0)
+                description_box.setVisibility(View.VISIBLE);
+            else
+                description_box.setVisibility(View.GONE);
+
+            count++;
+
+
+
         }
     }
 }
